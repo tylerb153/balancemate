5fcf2791f96cf9628dd7e3498916cbdf0befa046
Tyler Bischoff <tbischoff2@ymail.com>
2024-12-03 15:18:07 -0500

    Renamed App and window to fit project

--- lib/main.dart
+++ lib/main.dart
@@ -53,17 +53,49 @@ class _HomePageState extends State<HomePage> {
                   'Counterweight Distance: placeholder distance')
                 ),
                 Center(child: Image.asset("assets/Amazing and extremely cool telescope icon that's certainly not just dev art ;D.png")),
-                const Padding(padding: EdgeInsets.fromLTRB(0, 0, 32, 0), child: 
-                  Align(alignment: Alignment.centerRight, child:
-                    CircleAvatar(backgroundColor: Colors.red, child: Icon(Icons.search, color: Colors.white))
-                  )
-                )
+                // const Padding(padding: EdgeInsets.fromLTRB(0, 0, 32, 0), child: 
+                //   Align(alignment: Alignment.centerRight, child:
+                //     CircleAvatar(backgroundColor: Colors.red, child: Icon(Icons.search, color: Colors.white))
+                //   )
+                // )
                 // Align(alignment: Alignment.bottomCenter, child:
                 //   Container(height: 2, color: Colors.black)
                 // )
               ])
             )
           ),
+      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setup'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Details'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red[700],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class SetupPage extends StatefulWidget {
+  const SetupPage({super.key});
+  @override
+  State<SetupPage> createState() => _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text("Setup Page")),
       bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setup'),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
